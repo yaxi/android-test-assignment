2fc5f0eea960c89f628b8bf242ba95fe31d1dd2c
Yaxi Ye <yaxi@whereismytransport.com>
2023-09-29 00:22:10 +0100

    main activity to use NavHost

--- app/src/main/java/com/example/shacklehotelbuddy/MainActivity.kt
+++ app/src/main/java/com/example/shacklehotelbuddy/MainActivity.kt
@@ -3,15 +3,20 @@ package com.example.shacklehotelbuddy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.compose.rememberNavController
+import com.example.shacklehotelbuddy.navigation.NavigationRoutes
 import com.example.shacklehotelbuddy.search.ui.MainScreen
 import com.example.shacklehotelbuddy.ui.theme.ShackleHotelBuddyTheme
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : ComponentActivity() {
+@AndroidEntryPoint
+class MainActivity: ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ShackleHotelBuddyTheme {
-               MainScreen()
+                NavigationRoutes(navController = rememberNavController())
             }
         }
     }
